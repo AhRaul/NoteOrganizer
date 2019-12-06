28ac56e8c7c1717df4500f8675a001e262710c96
vladislav ershov <vldrshv@gmail.com>
2019-12-06 20:51:20 +0300

    search view

--- app/src/main/java/ru/reliableteam/noteorganizer/notes/view/NotesFragment.java
+++ app/src/main/java/ru/reliableteam/noteorganizer/notes/view/NotesFragment.java
@@ -2,17 +2,15 @@ package ru.reliableteam.noteorganizer.notes.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import ru.reliableteam.noteorganizer.R;
 import ru.reliableteam.noteorganizer.notes.presenter.INotesPresenter;
 import ru.reliableteam.noteorganizer.notes.presenter.NotesPresenter;
@@ -20,7 +18,8 @@ import ru.reliableteam.noteorganizer.notes.presenter.NotesPresenter;
 /**
  *  Note List Fragment
  */
-public class NotesFragment extends Fragment implements View.OnClickListener{
+public class NotesFragment extends Fragment implements View.OnClickListener {
+    private final String CLASS_TAG = "NotesFragment";
 
     private View root;
     private RecyclerView recyclerView;
@@ -34,6 +33,7 @@ public class NotesFragment extends Fragment implements View.OnClickListener{
 
         root = inflater.inflate(R.layout.fragment_notes, container, false);
         writeNewNote = root.findViewById(R.id.notes_write_fab);
+
         writeNewNote.setOnClickListener(this);
         presenter = new NotesPresenter(this);
 
@@ -42,11 +42,14 @@ public class NotesFragment extends Fragment implements View.OnClickListener{
         return root;
     }
 
+
     private void initRecyclerView() {
         recyclerView = root.findViewById(R.id.notes_rv);
         recyclerView.setHasFixedSize(false);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
         recyclerView.setAdapter(new MyAdapter(presenter));
+
+        recyclerView.addOnScrollListener(getRecyclerScrollListener());
     }
 
     public void notifyDataChanged() {
@@ -73,8 +76,28 @@ public class NotesFragment extends Fragment implements View.OnClickListener{
     public void onResume(){
         super.onResume();
         presenter.getNotes();
-//        presenter.notifyDatasetChanged();
         System.out.println("ON RESUME");
     }
 
+    private RecyclerView.OnScrollListener getRecyclerScrollListener() {
+        return new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int fabVisibility = writeNewNote.getVisibility();
+                if (dy != 0 && fabVisibility == View.VISIBLE)
+                    writeNewNote.hide();
+
+                Log.i(CLASS_TAG, "dy = " + dy);
+            }
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.i(CLASS_TAG, "new state = " + newState);
+                int fabVisibility = writeNewNote.getVisibility();
+                if (newState == 0 && fabVisibility != View.VISIBLE) // seemed to stop
+                    writeNewNote.show();
+            }
+        };
+    }
 }
