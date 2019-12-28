12eb1361acf3799cd7f9418d56ffd306e3765a39
vladislav ershov <vldrshv@gmail.com>
2019-12-28 22:05:50 +0300

    date picking

--- app/src/main/java/ru/reliableteam/noteorganizer/todos/AddTodoBottomFragment.java
+++ app/src/main/java/ru/reliableteam/noteorganizer/todos/AddTodoBottomFragment.java
@@ -1,36 +1,65 @@
 package ru.reliableteam.noteorganizer.todos;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.LinearLayoutCompat;
+
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Calendar;
+
 import ru.reliableteam.noteorganizer.R;
 import ru.reliableteam.noteorganizer.todos.view.TodosFragment;
+import ru.reliableteam.noteorganizer.utils.DateUtils;
 
 
 public class AddTodoBottomFragment extends BottomSheetDialogFragment {
     private final String CLASS_TAG = "NotesBtmDialogFragment";
+    private final String EMPTY_TEXT = "";
 
-    BottomSheetBehavior bottomSheetBehavior;
-    ImageButton cancelBtn;
-    ImageButton saveBtn;
+    private View root;
+    private BottomSheetBehavior bottomSheetBehavior;
 
-    TextInputEditText title, description;
+    private ImageButton cancelBtn;
+    private ImageButton saveBtn;
+    private TextInputEditText title, description;
+
+    // date picking and showing
+    private LinearLayoutCompat dateLayout;
+    private DatePickerDialog datePickerDialog;
+    private ImageButton dateBtn;
+    private TextView dateTv;
+    private ImageButton dateDelete;
+
+    // time picking and showing
+    private LinearLayoutCompat timeLayout;
+    private TimePickerDialog timePickerDialog;
+    private ImageButton timeBtn;
+    private TextView timeTv;
+    private ImageButton timeDelete;
+
+    // todo isDone
+    // todo sub-tusks
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -38,31 +67,111 @@ public class AddTodoBottomFragment extends BottomSheetDialogFragment {
 
         BottomSheetDialog bottomSheet = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
 
-//        inflating layout
-        View view = View.inflate(getContext(), R.layout.fragment_add_todo, null);
-//
-//         TODO: 21.12.2019 add todo saving
-//
-        cancelBtn = view.findViewById(R.id.cancel_button);
-        cancelBtn.setOnClickListener( v -> dismiss() );
-        saveBtn = view.findViewById(R.id.save_button);
-        saveBtn.setOnClickListener(v -> saveTodo());
-        title = view.findViewById(R.id.todo_title);
-        description = view.findViewById(R.id.todo_description);
+        root = View.inflate(getContext(), R.layout.fragment_add_todo, null);
 
-        bottomSheet.setContentView(view);
+        initUI();
 
-        bottomSheetBehavior = BottomSheetBehavior.from((View) (view.getParent()));
+        bottomSheet.setContentView(root);
+
+        bottomSheetBehavior = bottomSheet.getBehavior();//BottomSheetBehavior.from((View) (view.getParent()));
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+        bottomSheetBehavior.setBottomSheetCallback(getCallback());
 
         return bottomSheet;
     }
 
+    private void initUI() {
+        initDatePicking();
+        initTimePicking();
+        initCancel();
+        initSave();
+        title = root.findViewById(R.id.todo_title);
+        description = root.findViewById(R.id.todo_description);
+    }
+
+    private void initDatePicking() {
+        datePickerDialog = new DatePickerDialog(getContext());
+        dateBtn = root.findViewById(R.id.date_btn);
+        dateBtn.setOnClickListener(v -> datePickerDialog.show());
+        dateLayout = root.findViewById(R.id.date_layout);
+        dateTv = root.findViewById(R.id.todo_date);
+        dateDelete = root.findViewById(R.id.todo_date_delete);
+        dateDelete.setOnClickListener(v -> {
+            dateLayout.setVisibility(View.GONE);
+            dateTv.setText(EMPTY_TEXT);
+            dateBtn.setEnabled(true);
+        });
+        datePickerDialog.setOnDateSetListener(
+                (view, year, month, dayOfMonth) -> {
+                    dateLayout.setVisibility(View.VISIBLE);
+                    dateTv.setText(DateUtils.getNormalizedDate(dayOfMonth, month, year));
+                    dateBtn.setEnabled(false);
+                }
+        );
+    }
+    private void initTimePicking() {
+        timeBtn = root.findViewById(R.id.time_btn);
+        timeBtn.setOnClickListener(v -> timePickerDialog.show());
+        timeLayout = root.findViewById(R.id.time_layout);
+        timeTv = root.findViewById(R.id.todo_time);
+        timeDelete = root.findViewById(R.id.todo_time_delete);
+        timeDelete.setOnClickListener(v -> {
+            timeLayout.setVisibility(View.GONE);
+            timeTv.setText(EMPTY_TEXT);
+            timeBtn.setEnabled(true);
+        });
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    timeLayout.setVisibility(View.VISIBLE);
+                    timeTv.setText(DateUtils.getNormalizedTime(hourOfDay, minute));
+                    timeBtn.setEnabled(false);
+            }
+        };
+        timePickerDialog = new TimePickerDialog(getActivity(), onTimeSetListener, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+    private void initCancel() {
+        cancelBtn = root.findViewById(R.id.cancel_button);
+        cancelBtn.setOnClickListener( v -> dismiss() );
+    }
+    private void initSave() {
+        saveBtn = root.findViewById(R.id.save_button);
+        saveBtn.setOnClickListener(v -> saveTodo());
+    }
+
     private void saveTodo() {
-        Intent intent = new Intent();
-        intent.putExtra("title", title.getText().toString());
-        intent.putExtra("description", description.getText().toString());
-        System.out.println(getTargetRequestCode());
-        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+//        Intent intent = new Intent();
+        long dateTime = DateUtils.stringToDate(getTime(), getDate());
+        System.out.println(DateUtils.dateToString(dateTime));
+//        intent.putExtra("title", title.getText().toString());
+//        intent.putExtra("description", description.getText().toString());
+//        intent.putExtra("endDate", dateTime);
+//        System.out.println(getTargetRequestCode());
+//        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
         dismiss();
+    }
+
+    private String getTime() {
+        return timeTv.getText().toString();
+    }
+    private String getDate() {
+        return dateTv.getText().toString();
+    }
+
+    private BottomSheetBehavior.BottomSheetCallback getCallback() {
+        return new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN)
+                    dismiss();
+            }
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
+        };
     }
 }
--- app/src/main/java/ru/reliableteam/noteorganizer/utils/DateUtils.java
+++ app/src/main/java/ru/reliableteam/noteorganizer/utils/DateUtils.java
@@ -13,6 +13,9 @@ public class DateUtils {
     private final static int MINUTES = 60;
     private final static int HOURS = 24;
     private final static String DATE_OUTPUT_FORMAT = "%d/%d/%d";
+    private final static String TIME_OUTPUT_FORMAT = "%s : %s";
+    private final static String BEGIN_DATE = "1/0/1970";
+    private final static String BEGIN_TIME = "00 : 00";
 
     public static String dateToString(Long dateInMills) {
         Calendar calendar = getCalendar(dateInMills);
@@ -21,7 +24,63 @@ public class DateUtils {
         int month = calendar.get(Calendar.MONTH);
         int year = calendar.get(Calendar.YEAR);
 
+//  todo delete
+
+//        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+//        int minute = calendar.get(Calendar.MINUTE);
+
+        String date = getNormalizedDate(day, month, year);
+//        String time = getNormalizedTime(hour, minute);
+
+        return date;
+    }
+
+    public static Long stringToDate(String time, String date) {
+
+        Calendar calendar = getCalendar(0L);
+        if (!time.equals("")) {
+            Integer[] timeArr = parseTime(time);
+            calendar.set(Calendar.HOUR_OF_DAY, timeArr[0]);
+            calendar.set(Calendar.MINUTE, timeArr[1]);
+        }
+
+        if (!date.equals("")) {
+            Integer[] dateArr = parseDate(date);
+            calendar.set(Calendar.DAY_OF_MONTH, dateArr[0]);
+            calendar.set(Calendar.MONTH, dateArr[1]);
+            calendar.set(Calendar.YEAR, dateArr[2]);
+        }
+
+        return calendar.getTimeInMillis();
+    }
+
+
+    public static String getNormalizedTime(Integer hour, Integer min) {
+        return String.format(new Locale("ENG"), TIME_OUTPUT_FORMAT,
+                (hour == 0) ? "00" : hour + "",
+                (min == 0) ? "00" : min + ""
+        );
+    }
+    public static String getNormalizedDate(Integer day, Integer month, Integer year) {
         return String.format(new Locale("ENG"), DATE_OUTPUT_FORMAT, day, month, year);
+    }
+    private static Integer[] parseTime(String time) {
+        String[] timeStringArr = time.replace(" ", "").split(":");
+        Integer[] timeArr = new Integer[2];
+        if (timeStringArr.length > 0) {
+            for (int i = 0; i < timeStringArr.length; i++)
+                timeArr[i] = Integer.valueOf(timeStringArr[i]);
+        }
+        return timeArr;
+    }
+    private static Integer[] parseDate(String date) {
+        String[] dateStringArr = date.replace(" ", "").split("/");
+        Integer[] dateArr = new Integer[3];
+        if (dateStringArr.length > 0) {
+            for (int i = 0; i < dateStringArr.length; i++)
+                dateArr[i] = Integer.valueOf(dateStringArr[i]);
+        }
+        return dateArr;
     }
 
     private static Calendar getCalendar(Long dateInMills) {
